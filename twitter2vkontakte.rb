8abef5d1ba6e77307698ac56bf4b877044d3f04f
Mike Lapshin <sotakone@sotakone.com>
2009-09-20 17:50:05 +0400

    now we login to vkontakte only if we have statuses to repost

--- twitter2vkontakte.rb
+++ twitter2vkontakte.rb
@@ -11,31 +11,32 @@ require 'iconv'
 
 def vkontakte_login(email, password)
   res = Net::HTTP.post_form(URI.parse("http://login.vk.com/"), { "email" => email, "pass" => password, "vk" => '', "act" => 'login' })
-  res.body.match(/value='([a-z0-9]+)'/)[1]
+  sid = res.body.match(/value='([a-z0-9]+)'/)[1]
+  raise "Vkontakte.ru authentication failed" if sid == 'nonenone'
+
+  return sid
 end
 
 def vkontakte_set_status(txt, sid)
   f = open("http://vkontakte.ru", "Cookie" => "remixsid=#{sid}").read.to_s
-  f =~ /<input type='hidden' id='activityhash' value='([^']+)'>/
 
   url = URI.parse("http://vkontakte.ru/profile.php")
   request = Net::HTTP::Post.new(url.path)
 
-  request.set_form_data({'setactivity' => txt, 'activityhash' => $1 })
+  request.set_form_data({'setactivity' => txt, 'activityhash' => f.match(/<input type='hidden' id='activityhash' value='([^']+)'>/)[1] })
   request['cookie'] = "remixsid=#{sid}"
-  request['referer'] = 'http://vkontakte.ru/id145958'
 
   res = Net::HTTP.new(url.host, url.port).start { |http| http.request(request) }
 end
 
 config = YAML.load_file("config.yaml")
-sid = vkontakte_login(config[:vkontakte][:email], config[:vkontakte][:password])
 
 last_run_time = Time.at(open("last_run_time.txt").read.to_s.to_i) rescue nil
 json = open("http://twitter.com/statuses/user_timeline.json?screen_name=#{config[:twitter][:screen_name]}&count=#{last_run_time.nil? ? 1 : 5}").read.to_s
 
 posts = JSON.parse(json)
 last_post_created_at = nil
+sid = nil
 
 posts.each do |p|
   created_at = Time.parse(p['created_at'])
@@ -44,6 +45,7 @@ posts.each do |p|
     status = "[twitter] " + p['text']
     status = status[0...157] + "..." if status.length > 160
 
+    sid ||= vkontakte_login(config[:vkontakte][:email], config[:vkontakte][:password])
     vkontakte_set_status(status, sid)
     puts "Status set: #{status}"
 
@@ -51,4 +53,4 @@ posts.each do |p|
   end
 end
 
-File.open("last_run_time.txt", "w") { |f| f.write(last_post_created_at.to_i) }
+File.open("last_run_time.txt", "w") { |f| f.write(last_post_created_at.to_i) } if last_post_created_at
